0c10d90c9af577bb80efb0cd5fd2bc328d46737f
rcell <innkly@163.com>
2024-01-19 23:09:54 +0800

    update

--- infer_one.py
+++ infer_one.py
@@ -1,12 +1,13 @@
 from inference.DAV import *
-
+import torch.nn.functional as F
 if __name__ == "__main__":
 
-    weights_path = "runs/baseline/best/dac/weights.pth"
-    audio_path = "data/segments/wavs/2020000787.wav"
-    device = 'cuda:0'
+    weights_path = "runs/vqfixed/best/dac/weights.pth"
+    audio_path = "测试音频.wav"
+    device = 'cuda:1'
     model = load_model(weights_path, device)
 
     z = encode_from_file(model, audio_path)
+    z = F.interpolate(z[None], size=int(z.shape[-1]*1.1), mode='linear', align_corners=True)[0]
     print("z shape:", z.shape)
     decode_to_file(model, z, 'out.wav')
